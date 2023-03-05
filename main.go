0658116a1d5fe978719c7440a158c3a606e1a201
Dylan Gillespie <dylantannergillespie@gmail.com>
2023-03-05 11:52:05 -0500

    Moved loadConfigurationSettings() to main.go

--- main.go
+++ main.go
@@ -10,5 +10,11 @@ var staticFileDirectory = http.Dir("./static")
 
 func main() {
 
+	//config := loadConfigurationSettings()
+
 	webServer.Initialize(port, staticFileDirectory)
+}
+
+func loadConfigurationSettings() {
+	//https:\\ini.unknwon.io/docs/intro/getting_started
 }
